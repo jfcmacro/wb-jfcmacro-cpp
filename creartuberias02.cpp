84b3b68cc4694ff7d51371b76a6257899626dd96
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2016-04-22 19:55:18 +0000

    Adding pipes examples

---
+++ creartuberias02.cpp
@@ -0,0 +1,59 @@
+/*
+ * fichero: Este ejemplo crea un tuberia y pasa un mensaje que es un entero
+ *          que sera incrementado por el segundo proceso
+ */
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <sys/wait.h>
+
+using namespace std;
+
+const int START { 10 };
+
+int
+main(int argc, const char *argv[]) {
+
+  int number { START };
+  
+  if (argc == 2) {
+
+    string start {argv[1] };
+    
+    number = stoi(start);
+  }
+  
+  int tuberia1[2];
+  int tuberia2[2];
+
+  pipe(tuberia1);
+  pipe(tuberia2);
+
+  if (fork() == 0) { // inputIncr
+
+    close(tuberia1[1]);
+    close(tuberia2[0]);
+    dup2(tuberia1[0], 0);
+    dup2(tuberia2[1], 1);
+    execl("./inputIncr", "inputIncr", "2", NULL);
+  }
+  else { // Me
+    
+    close(tuberia1[0]);
+    close(tuberia2[1]);
+    dup2(tuberia1[1], 1);
+    dup2(tuberia2[0], 0);
+
+    do {
+      cerr << number << endl;
+      cout << number;
+      
+    } while (cin >> number);
+  }
+
+  int status;
+
+  wait(&status);
+  
+  return 0;
+}
---
+++ inputIncr.cpp
@@ -0,0 +1,33 @@
+/*
+ * fichero: incrEntrada.cpp
+ *
+ * proposito: Lee un valor de la entrada estandar y lo incrementa
+ *            hasta que encuentre un fin de fichero (eof).
+ */
+#include <iostream>
+#include <string>
+
+const int OFFSET { 1 };
+
+int
+main(int argc, char *argv[]) {
+
+  int incrValue = OFFSET;
+  
+  if (argc == 2) {
+    std::string value { argv[1] };
+    
+    incrValue = std::stoi(value);
+  }
+
+  int number;
+
+  while (std::cin >> number) {
+
+    number += incrValue;
+    
+    std::cout << number;
+  }
+
+  return 0;
+}
