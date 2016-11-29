6208e379b5fc4cf25a0fe15978d7ebc6c9da5bcf
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2016-11-29 17:14:55 +0000

    Adding missing files

---
+++ process.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class  ProgramInfo {
+public:
+  ProgramInfo(string cmd,
+              vector<string> args);
+  ~ProgramInfo();
+  string getCmd() const;
+  vector<string> getArgs() const;
+  string getArg(unsigned int pos) const;
+  unsigned int getNroArgs() const;
+private:
+  string cmd;
+  vector<string> args;
+};
+
+/* class ChainedProcessInfo { */
+/*  public: */
+/*   /\* ChainedProcessInfo(string& inFileName, string& outFileName); *\/ */
+/*   /\* ChainedProcessInfo(string& fileName, bool isIn); *\/ */
+/*   ChainedProcessInfo(); */
+/*   ~ChainedProcessInfo(); */
+/*   int[] getInOut(); */
+/*   void setOutIn(); */
+/*  private: */
+/*   bool active; */
+/*   int in; */
+/*   int out; */
+/*   int pipeIn[2]; */
+/*   int pipeOut[2]; */
+/* }; */
+
+char* createCopyChar(const string& arg);
+
+void closingArrayPipes(int **pipes, int n, int noClose);
+void closingArrayPipesExcEdges(int **pipes, int n);
+
+
+void createProcessPipe(const vector<ProgramInfo>& programs,
+                       const int pipeIn[2],
+                       const int pipeOut[2],
+                       vector<pid_t>& pidList);
+
+enum ChainedPipeProcessException { CPPE_INPUT_FILE_DOES_NOT_EXIST,
+                                   CPPE_OUTPUT_FILE_EXITS,
+                                   CPPE_INPUT_FILE_IS_NOT_REGULAR,
+                                   CPPE_INPUT_FILE_HAS_NOT_PERMISSIONS,
+                                   CPPE_INPUT_FILE_NOT_OPEN
+};
+
+void createChainedPipeProcess(const vector<ProgramInfo>& programs,
+                              const string& inFileName,
+                              const string& outFileName,
+                              int inOut[2],
+                              vector<pid_t>& pidList);
+
+char* createCopyChar(const string& arg);
