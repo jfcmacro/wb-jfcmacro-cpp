db98f8f5f483991da8a4b634f959a9afffe9a396
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2022-10-17 17:09:50 +0000

    Adding some files

--- boost/example.cpp
+++ boost/example.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <cstdlib>
 
 int main()
 {
   using namespace boost::lambda;
   typedef std::istream_iterator<int> in;
 
-  std::for_each(
-		in(std::cin), in(), std::cout << (_1 * 3) << " " );
+  std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " " );
+
+  return EXIT_SUCCESS;
 }
